f01626bb83c703784aa7017c190b5ba52ce1cd72
drewm <drew.mclellan@gmail.com>
2019-01-30 13:47:44 +0000

    Simplifying dependency checks

--- src/Drip.php
+++ src/Drip.php
@@ -121,58 +121,72 @@ class Drip
      */
     protected function makeRequest($http_verb, $api_method, $args = [], $timeout = 10, $url = null)
     {
+        $this->checkDependencies();
+
         $url = $this->constructRequestUrl($url, $api_method);
 
-        if (function_exists('curl_init') && function_exists('curl_setopt')) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept: application/vnd.api+json',
+            'Content-Type: application/vnd.api+json',
+        ]);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'DrewM/Drip (github.com/drewm/drip)');
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->token . ': ');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $this->verify_ssl);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+        curl_setopt($ch, CURLOPT_URL, $url);
 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, [
-                'Accept: application/vnd.api+json',
-                'Content-Type: application/vnd.api+json',
-            ]);
-            curl_setopt($ch, CURLOPT_USERAGENT, 'DrewM/Drip (github.com/drewm/drip)');
-            curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-            curl_setopt($ch, CURLOPT_USERPWD, $this->token . ': ');
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $this->verify_ssl);
-            curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
-            curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
-            curl_setopt($ch, CURLOPT_URL, $url);
+        switch ($http_verb) {
+            case 'post':
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
+                break;
 
-            switch ($http_verb) {
-                case 'post':
-                    curl_setopt($ch, CURLOPT_POST, 1);
-                    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
-                    break;
+            case 'get':
+                $query = http_build_query($args);
+                curl_setopt($ch, CURLOPT_URL, $url . '?' . $query);
+                break;
 
-                case 'get':
-                    $query = http_build_query($args);
-                    curl_setopt($ch, CURLOPT_URL, $url . '?' . $query);
-                    break;
+            case 'delete':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+                break;
+        }
 
-                case 'delete':
-                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
-                    break;
-            }
+        $result = curl_exec($ch);
 
-            $result = curl_exec($ch);
-
-            if (!curl_errno($ch)) {
-                $info = curl_getinfo($ch);
-                curl_close($ch);
-                return new Response($info, $result);
-            }
-
-            $errno = curl_errno($ch);
-            $error = curl_error($ch);
-
+        if (!curl_errno($ch)) {
+            $info = curl_getinfo($ch);
             curl_close($ch);
+            return new Response($info, $result);
+        }
 
-            throw new DripException($error, $errno);
-        } else {
+        $errno = curl_errno($ch);
+        $error = curl_error($ch);
+
+        curl_close($ch);
+
+        throw new DripException($error, $errno);
+
+    }
+
+    /**
+     * Check for required PHP functionality
+     *
+     * @return bool
+     * @throws DripException
+     */
+    private function checkDependencies()
+    {
+        if (!function_exists('curl_init') || !function_exists('curl_setopt')) {
             throw new DripException("cURL support is required, but can't be found.", 1);
         }
+
+        return true;
     }
+
 
     /**
      * @param string|null $url
